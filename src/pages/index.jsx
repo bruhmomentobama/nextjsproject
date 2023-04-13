4f78cff0ccd9fb4283b7eb4b8eb9493d6f981e5f
Yoshi Grimbal <YOSHIGRIMBAL@GMAIL.COM>
2023-04-13 15:11:27 +0800

    something good i guess

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,20 +5,31 @@ export default function Home() {
   const [articles, setArticles] = useState([])
   const [page, setPage] = useState(1)
   const targetRef = useRef(null)
-  const [country, setCountry] = useState('')
+  const [source, setSource] = useState('')
+  const API_KEY = '8e2111a360824635a170e0ba8f06f179'
 
   useEffect(() => {
     async function fetchData() {
-      const url = `https://content.guardianapis.com/world/${country}?api-key=35930e1c-07e6-49e2-a899-304bfe7015f4&show-fields=thumbnail&page=${page}`
+      let url = `https://newsapi.org/v2/top-headlines?language=en&apiKey=${API_KEY}&pageSize=20&page=${page}`
+      if (source) {
+        url += `&sources=${source}`
+      }
 
       const response = await fetch(url)
       const data = await response.json()
-      setArticles((prevArticles) => [...prevArticles, ...data.response.results])
+
+      if (data.articles) {
+        const articlesWithImages = data.articles.filter((article) => article.urlToImage)
+        setArticles((prevArticles) => [...prevArticles, ...articlesWithImages])
+      }
     }
 
     setArticles([])
-    fetchData()
-  }, [country, page])
+    setPage(1)
+    if (source) {
+      fetchData()
+    }
+  }, [source, page, API_KEY])
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -41,41 +52,83 @@ export default function Home() {
     }
   }, [targetRef])
 
-  function handleCountryClick(country) {
-    const encodedCountry = encodeURIComponent(country)
-    setCountry(encodedCountry)
-    setPage(1)
+  function handleSourceClick(sourceId) {
+    setSource(sourceId)
   }
 
   return (
     <div>
-      <h1 className="text-center my-5">The Guardian</h1>
-      <nav className={`${styles.navBar}`}>
-        <button type="button" onClick={() => handleCountryClick('Argentina')}>Argentina</button>
-        <button type="button" onClick={() => handleCountryClick('Chile')}>Chile</button>
-        <button type="button" onClick={() => handleCountryClick('China')}>China</button>
-        <button type="button" onClick={() => handleCountryClick('United Kingdom')}>United Kingdom</button>
+      <h1 className="text-center my-5 custom-font" style={{ color: 'white' }}>
+        World Trending News
+      </h1>
+
+      <nav className={`d-flex justify-content-center ${styles.navBar}`}>
+        <button type="button" onClick={() => handleSourceClick('abc-news')} className={`${styles.navButton} custom-font button-hover`}>
+          ABC News
+        </button>
+        <button type="button" onClick={() => handleSourceClick('bbc-news')} className={`${styles.navButton} custom-font button-hover`}>
+          BBC News
+        </button>
+        <button type="button" onClick={() => handleSourceClick('associated-press')} className={`${styles.navButton} custom-font button-hover`}>
+          Associated Press
+        </button>
+        <button type="button" onClick={() => handleSourceClick('business-insider')} className={`${styles.navButton} custom-font button-hover`}>
+          Business Insider
+        </button>
+        <button type="button" onClick={() => handleSourceClick('the-wall-street-journal')} className={`${styles.navButton} custom-font button-hover`}>
+          The Wall Street Journal
+        </button>
+        <button type="button" onClick={() => handleSourceClick('cbs-news')} className={`${styles.navButton} custom-font button-hover`}>
+          CBS
+        </button>
+        <button type="button" onClick={() => handleSourceClick('cnn')} className={`${styles.navButton} custom-font button-hover`}>
+          CNN
+        </button>
+        <button type="button" onClick={() => handleSourceClick('ign')} className={`${styles.navButton} custom-font button-hover`}>
+          IGN
+        </button>
       </nav>
-      {articles && articles.length > 0 ? (
-        <ul className={`${styles.articlesList}`}>
-          {articles.map((article, index) => (
-            <li
-              key={article.id}
-              className={`${styles.articleItem} ${index % 3 === 2 ? styles.lastInRow : ''}`}
-            >
-              <a href={article.webUrl} target="_blank" rel="noopener noreferrer">
-                {article.fields?.thumbnail && (
-                  <img src={article.fields.thumbnail} alt={article.webTitle} className={`${styles.articleImage}`} />
-                )}
-                <h5>{article.webTitle}</h5>
-              </a>
-            </li>
-          ))}
-          <li ref={targetRef} />
-        </ul>
-      ) : (
-        <p>Loading articles...</p>
-      )}
+
+      <div className="container border" style={{ backgroundColor: 'white' }}>
+        {source ? (
+          articles && articles.length > 0 ? (
+            <ul className={`${styles.articlesList}`}>
+              {articles.map((article, index) => (
+                <li
+                  key={article.url}
+                  className={`${styles.articleItem} ${
+                    index % 3 === 2 ? styles.lastInRow : ''
+                  }`}
+                >
+                  <a
+                    href={article.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: 'none' }}
+                  >
+                    {article.urlToImage && (
+                    <img
+                      src={article.urlToImage}
+                      alt={article.title}
+                      className={`${styles.articleImage}`}
+                      style={{ marginTop: '20px' }}
+                    />
+                    )}
+                    <h5 style={{ textAlign: 'center' }}>{article.title}</h5>
+                  </a>
+                </li>
+              ))}
+
+              <li ref={targetRef} />
+            </ul>
+          ) : (
+            <h2 className={`${styles.loadingMessage} text-center`}>Loading articles...</h2>
+          )
+        ) : (
+          <h2 className={`${styles.loadingMessage} text-center`}>Select a news outlet provider to view their articles.</h2>
+        )}
+      </div>
+
     </div>
   )
 }
